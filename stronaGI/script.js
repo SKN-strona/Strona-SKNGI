b1fb34a36b7e27e1d99bef86afc7a67d32ac2542
Ceglee <ceglarzp@gmail.com>
2014-12-05 20:43:43 +0100

    Ceglee działa dalej
    Menu się wysuwa co dalej?

--- stronaGI/script.js
+++ stronaGI/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 	
 	$('.spotkanie').mouseenter(function(){
-    $('.spotkanie').css('height' ,'300px');
+		$('.spotkanie').css('height' ,'300px');
+	});
+	$('.spotkanie').mouseleave(function(){
+		$('.spotkanie').css('height' ,'100px');
+	});
+	$('.dropdown').mouseenter(function(){
+		$(this).children('.dropdown-menu').show();
+	});
+	$('.dropdown').mouseleave(function(){
+		$(this).children('.dropdown-menu').hide();
 	});
 });
 	
